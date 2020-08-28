aeb3159dc8a23a455dc5339ff87bbd3b156279e8
vinnyfs89 <viniciusfesil@gmail.com>
2020-08-28 18:10:16 -0300

    ✨ Checkin / Checkout Commands

--- cypress/integration/checkin_2.js
+++ cypress/integration/checkin_2.js
@@ -1,10 +1,10 @@
-describe('Checkin', () => {
+describe('Checkin 2º Turno', () => {
   it('login', () => {
     const user = Cypress.env('COOPERSYSTEM_USERNAME');
     const pass = Cypress.env('COOPERSYSTEM_PASSWORD');
     cy.visit('http://edesenv3.coopersystem.com.br/edesenv2/');
     cy.get('input[id=username]').type(`${user}`);
     cy.get('input[id=password]').type(`${pass}{enter}`);
-    cy.get('input[id=entrada_2]').click();
+    cy.get('button[id=entrada_2]').click();
   });
 });
--- cypress/integration/checkout_2.js
+++ cypress/integration/checkout_2.js
@@ -1,10 +1,10 @@
-describe('Checkin', () => {
+describe('Checkout 2º Turno', () => {
   it('login', () => {
     const user = Cypress.env('COOPERSYSTEM_USERNAME');
     const pass = Cypress.env('COOPERSYSTEM_PASSWORD');
     cy.visit('http://edesenv3.coopersystem.com.br/edesenv2/');
     cy.get('input[id=username]').type(`${user}`);
     cy.get('input[id=password]').type(`${pass}{enter}`);
-    cy.get('input[id=saida_2]').click();
+    cy.get('button[id=saida_2]').click();
   });
 });
